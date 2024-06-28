46487d1712c7e5008ea58758b0c8d87732906b68
AI_Nomads <chugue85@gmail.com>
2024-06-28 22:59:30 +0900

    savepoint

--- src/main/java/com/project/videodemo/video/Video.java
+++ src/main/java/com/project/videodemo/video/Video.java
@@ -1,4 +1,26 @@
 package com.project.videodemo.video;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "video_tb")
 public class Video {
+    @Id
+    private Integer id;
+    private String title;
+    private String videoPath;
+
+    @Builder
+    public Video(Integer id, String title, String videoPath) {
+        this.id = id;
+        this.title = title;
+        this.videoPath = videoPath;
+    }
 }
